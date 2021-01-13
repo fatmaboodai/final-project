eafcf3bfc5ff1ed8999fbdf1997ae9c9837416a4
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2021-01-14 01:55:01 +0300

    final project

---
+++ main.js
@@ -0,0 +1,379 @@
+
+function Pizza() {
+    var x = document.getElementById('pancakes');
+    var y = document.getElementById('pasta');
+    var z = document.getElementById('salad');
+    var b =  document.getElementById('burger');
+    var A = document.getElementById('acai');
+    var t = document.getElementById('toast');
+    var L =document.getElementById('lasagna')
+
+    if ( x.style.display === 'none') {
+      x.style.display = 'block';}
+    else {
+        x.style.display = 'none';
+      }
+      if ( y.style.display === 'none') {
+        y.style.display = 'block';}
+      else {
+          y.style.display = 'none';
+        }
+        if ( z.style.display === 'none') {
+            z.style.display = 'block';}
+          else {
+              z.style.display = 'none';
+            }
+            if ( b.style.display === 'none') {
+                b.style.display = 'block';}
+              else {
+                  b.style.display = 'none';
+                }
+                if ( A.style.display === 'none') {
+                    A.style.display = 'block';}
+                  else {
+                      A.style.display = 'none';
+                    }
+                      if ( t.style.display === 'none') {
+                            t.style.display = 'block';}
+                      else {
+                           t.style.display = 'none';
+                                                   }
+                          if ( L.style.display === 'none') {
+                             L.style.display = 'block';}
+                                 else {L.style.display = 'none';
+                                                                           }
+
+      
+    }
+    function Pancakes(){
+        var x = document.getElementById('pizza');
+        var y = document.getElementById('pasta');
+        var z = document.getElementById('salad');
+        var b =  document.getElementById('burger');
+        var A = document.getElementById('acai');
+        var t = document.getElementById('toast');
+        var L =document.getElementById('lasagna')
+    
+        if ( x.style.display === 'none') {
+          x.style.display = 'block';}
+        else {
+            x.style.display = 'none';
+          }
+        
+        
+          if ( y.style.display === 'none') {
+            y.style.display = 'block';}
+          else {
+              y.style.display = 'none';
+            }
+            if ( z.style.display === 'none') {
+                z.style.display = 'block';}
+              else {
+                  z.style.display = 'none';
+                }
+                if ( b.style.display === 'none') {
+                    b.style.display = 'block';}
+                  else {
+                      b.style.display = 'none';
+                    }
+                    if ( A.style.display === 'none') {
+                        A.style.display = 'block';}
+                      else {
+                          A.style.display = 'none';
+                        }
+                          if ( t.style.display === 'none') {
+                                t.style.display = 'block';}
+                          else {
+                               t.style.display = 'none';
+                                                       }
+                              if ( L.style.display === 'none') {
+                                 L.style.display = 'block';}
+                                     else {L.style.display = 'none';
+                                                                               }
+
+
+    }
+    function Burgers(){
+        var x = document.getElementById('pizza');
+        var y = document.getElementById('pasta');
+        var z = document.getElementById('salad');
+        var b =  document.getElementById('pancakes');
+        var A = document.getElementById('acai');
+        var t = document.getElementById('toast');
+        var L =document.getElementById('lasagna')
+    
+        if ( x.style.display === 'none') {
+          x.style.display = 'block';}
+        else {
+            x.style.display = 'none';
+          }
+        
+        
+          if ( y.style.display === 'none') {
+            y.style.display = 'block';}
+          else {
+              y.style.display = 'none';
+            }
+            if ( z.style.display === 'none') {
+                z.style.display = 'block';}
+              else {
+                  z.style.display = 'none';
+                }
+                if ( b.style.display === 'none') {
+                    b.style.display = 'block';}
+                  else {
+                      b.style.display = 'none';
+                    }
+                    if ( A.style.display === 'none') {
+                        A.style.display = 'block';}
+                      else {
+                          A.style.display = 'none';
+                        }
+                          if ( t.style.display === 'none') {
+                                t.style.display = 'block';}
+                          else {
+                               t.style.display = 'none';
+                                                       }
+                              if ( L.style.display === 'none') {
+                                 L.style.display = 'block';}
+                                     else {L.style.display = 'none';}}
+
+
+ function Salads(){
+    var x = document.getElementById('pizza');
+    var y = document.getElementById('pasta');
+    var z = document.getElementById('burger');
+    var b =  document.getElementById('pancakes');
+    var A = document.getElementById('acai');
+    var t = document.getElementById('toast');
+    var L =document.getElementById('lasagna')
+
+    if ( x.style.display === 'none') {
+      x.style.display = 'block';}
+    else {
+        x.style.display = 'none';
+      }
+    
+    
+      if ( y.style.display === 'none') {
+        y.style.display = 'block';}
+      else {
+          y.style.display = 'none';
+        }
+        if ( z.style.display === 'none') {
+            z.style.display = 'block';}
+          else {
+              z.style.display = 'none';
+            }
+            if ( b.style.display === 'none') {
+                b.style.display = 'block';}
+              else {
+                  b.style.display = 'none';
+                }
+                if ( A.style.display === 'none') {
+                    A.style.display = 'block';}
+                  else {
+                      A.style.display = 'none';
+                    }
+                      if ( t.style.display === 'none') {
+                            t.style.display = 'block';}
+                      else {
+                           t.style.display = 'none';
+                                                   }
+                          if ( L.style.display === 'none') {
+                             L.style.display = 'block';}
+                                 else {L.style.display = 'none';}
+
+ }                                
+ function Acai(){
+    var x = document.getElementById('pizza');
+        var y = document.getElementById('pasta');
+        var z = document.getElementById('salad');
+        var b =  document.getElementById('pancakes');
+        var A = document.getElementById('burger');
+        var t = document.getElementById('toast');
+        var L =document.getElementById('lasagna')
+    
+        if ( x.style.display === 'none') {
+          x.style.display = 'block';}
+        else {
+            x.style.display = 'none';
+          }
+        
+        
+          if ( y.style.display === 'none') {
+            y.style.display = 'block';}
+          else {
+              y.style.display = 'none';
+            }
+            if ( z.style.display === 'none') {
+                z.style.display = 'block';}
+              else {
+                  z.style.display = 'none';
+                }
+                if ( b.style.display === 'none') {
+                    b.style.display = 'block';}
+                  else {
+                      b.style.display = 'none';
+                    }
+                    if ( A.style.display === 'none') {
+                        A.style.display = 'block';}
+                      else {
+                          A.style.display = 'none';
+                        }
+                          if ( t.style.display === 'none') {
+                                t.style.display = 'block';}
+                          else {
+                               t.style.display = 'none';
+                                                       }
+                              if ( L.style.display === 'none') {
+                                 L.style.display = 'block';}
+                                     else {L.style.display = 'none';}
+ }
+
+ function Pasta(){
+    var x = document.getElementById('pizza');
+    var y = document.getElementById('burger');
+    var z = document.getElementById('salad');
+    var b =  document.getElementById('pancakes');
+    var A = document.getElementById('acai');
+    var t = document.getElementById('toast');
+    var L =document.getElementById('lasagna')
+
+    if ( x.style.display === 'none') {
+      x.style.display = 'block';}
+    else {
+        x.style.display = 'none';
+      }
+    
+    
+      if ( y.style.display === 'none') {
+        y.style.display = 'block';}
+      else {
+          y.style.display = 'none';
+        }
+        if ( z.style.display === 'none') {
+            z.style.display = 'block';}
+          else {
+              z.style.display = 'none';
+            }
+            if ( b.style.display === 'none') {
+                b.style.display = 'block';}
+              else {
+                  b.style.display = 'none';
+                }
+                if ( A.style.display === 'none') {
+                    A.style.display = 'block';}
+                  else {
+                      A.style.display = 'none';
+                    }
+                      if ( t.style.display === 'none') {
+                            t.style.display = 'block';}
+                      else {
+                           t.style.display = 'none';
+                                                   }
+                          if ( L.style.display === 'none') {
+                             L.style.display = 'block';}
+                                 else {L.style.display = 'none';}
+
+ }
+ function Toast(){
+    var x = document.getElementById('pizza');
+    var y = document.getElementById('pasta');
+    var z = document.getElementById('salad');
+    var b =  document.getElementById('pancakes');
+    var A = document.getElementById('acai');
+    var t = document.getElementById('burger');
+    var L =document.getElementById('lasagna')
+
+    if ( x.style.display === 'none') {
+      x.style.display = 'block';}
+    else {
+        x.style.display = 'none';
+      }
+    
+    
+      if ( y.style.display === 'none') {
+        y.style.display = 'block';}
+      else {
+          y.style.display = 'none';
+        }
+        if ( z.style.display === 'none') {
+            z.style.display = 'block';}
+          else {
+              z.style.display = 'none';
+            }
+            if ( b.style.display === 'none') {
+                b.style.display = 'block';}
+              else {
+                  b.style.display = 'none';
+                }
+                if ( A.style.display === 'none') {
+                    A.style.display = 'block';}
+                  else {
+                      A.style.display = 'none';
+                    }
+                      if ( t.style.display === 'none') {
+                            t.style.display = 'block';}
+                      else {
+                           t.style.display = 'none';
+                                                   }
+                          if ( L.style.display === 'none') {
+                             L.style.display = 'block';}
+                                 else {L.style.display = 'none';}
+ }
+
+ function Lasagna(){
+    var x = document.getElementById('pizza');
+    var y = document.getElementById('pasta');
+    var z = document.getElementById('salad');
+    var b =  document.getElementById('pancakes');
+    var A = document.getElementById('acai');
+    var t = document.getElementById('toast');
+    var L =document.getElementById('burger')
+
+    if ( x.style.display === 'none') {
+      x.style.display = 'block';}
+    else {
+        x.style.display = 'none';
+      }
+    
+    
+      if ( y.style.display === 'none') {
+        y.style.display = 'block';}
+      else {
+          y.style.display = 'none';
+        }
+        if ( z.style.display === 'none') {
+            z.style.display = 'block';}
+          else {
+              z.style.display = 'none';
+            }
+            if ( b.style.display === 'none') {
+                b.style.display = 'block';}
+              else {
+                  b.style.display = 'none';
+                }
+                if ( A.style.display === 'none') {
+                    A.style.display = 'block';}
+                  else {
+                      A.style.display = 'none';
+                    }
+                      if ( t.style.display === 'none') {
+                            t.style.display = 'block';}
+                      else {
+                           t.style.display = 'none';
+                                                   }
+                          if ( L.style.display === 'none') {
+                             L.style.display = 'block';}
+                                 else {L.style.display = 'none';}
+
+
+
+
+
+
+
+
+
+ }
